f2c45780775926ed383f11ca7e4b546a6bb738de
namrouche993 mhuv pc <namrouche993@gmail.com>
2024-03-17 15:19:23 +0100

    add index startend headers cells ...

--- get_columnwidths.js
+++ get_columnwidths.js
@@ -14,6 +14,30 @@ async function readColumnWidths(filePath) {
   // Get the length of the rows (assuming data doesn't have empty rows in between)
   const lengthRows = worksheet.actualRowCount;
 
+  let indexRStart;
+  let indexREndTitles;
+  let indexRStartHeaders;
+  let indexREndHeaders;
+  let indexRStartCells;
+  let indexREndCellsDisplayed;
+
+
+  for (let i = 1; i <= lengthRows; i++) {
+    if (worksheet.getRow(i).getCell(1).value === 'r_start') {
+        indexRStart = i;
+    } else if (worksheet.getRow(i).getCell(1).value === 'r_end_titles'){
+        indexREndTitles = i;
+    } else if (worksheet.getRow(i).getCell(1).value === 'r_start_headers'){
+        indexRStartHeaders = i;
+    } else if (worksheet.getRow(i).getCell(1).value === 'r_end_headers'){
+        indexREndHeaders = i;
+    } else if (worksheet.getRow(i).getCell(1).value === 'r_start_cells'){
+        indexRStartCells = i;
+    } else if (worksheet.getRow(i).getCell(1).value === 'r_end_cells_displayed'){
+        indexREndCellsDisplayed = i;
+    }
+  }
+
   const columnInformation = worksheet.columns.map((column, index) => {
     const type_col = worksheet.getRow(2).getCell(index + 1).value;
     const width_exact = worksheet.getRow(3).getCell(index + 1).value;
@@ -28,8 +52,15 @@ async function readColumnWidths(filePath) {
   });
 
   const columnWidths = {
+    index_r_start: indexRStart, // The row index where "r_start" is found in the first column
+    index_end_titles:indexREndTitles,
+    index_start_headers:indexRStartHeaders,
+    index_end_headers:indexREndHeaders,
+    index_start_cells:indexRStartCells,
+    index_end_cells_displayed:indexREndCellsDisplayed,
+
     length_rows: lengthRows,
-    information_of_columns: columnInformation
+    //information_of_columns: columnInformation
   };
 
   console.log('columnWidths :::::::: ');

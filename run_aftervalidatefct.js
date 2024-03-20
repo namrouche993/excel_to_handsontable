e08a87b5f851a90ae4cbb6120f7f124cae646a98
namrouche993 mhuv pc <namrouche993@gmail.com>
2024-03-20 14:58:39 +0100

    add validation_function to generate a js file of aftervalidatefct....

--- run_aftervalidatefct.js
+++ run_aftervalidatefct.js
@@ -39,7 +39,82 @@ async function generateValidationScript(excelFilePath) {
     }
   }
 
-  let functionStr = `function afterValidatefct(isValid, oldvalue, row, prop, source, hot, userLocale, decimalSeparator, navigator_language, use_english_date_by_user_himeself_in_modal, commentsPlugin, isLoading, setNotification) {\n`;
+  const importsAndExports = `import {
+    
+    last_row_after_header,
+  
+    currencyht_nbnb,currencyht_toshow_nbnb,afterdigit_nbnb, smallafterdigit_nbnb, afterdigitsmallnb_nbnb,
+    bignb_nbnb,smallnb_nbnb,decimalnumbers_toshow_withoutrenderer_innumbers_nbnb,usegrouping_nbnb_if_true,
+    is_negativenb_accepted_nbnb,
+    // is_float_accepted_nbnb,
+  
+    currencyht_intint,currencyht_toshow_intint,afterdigit_intint,smallafterdigit_intint,afterdigitsmallnb_intint,
+    bignb_intint,smallnb_intint,decimalnumbers_toshow_withoutrenderer_innumbers_intint,usegrouping_intint_if_true,
+    is_negativenb_accepted_intint,is_float_accepted_intint,
+  
+    afterdigit_percentage_percperc,smallafterdigit_percentage_percperc,afterdigitsmallnb_percentage_percperc,
+    bignbpercent_percperc,smallnbpercent_percperc,decimalnumbers_toshow_withoutrenderer_inpercentage_percperc,
+    is_negativenb_accepted_percperc,is_float_accepted_percperc,
+  
+    emails_length_em,
+    phonenumbers_length_pn,
+    onlynumbers_length_on,
+    text_length_txt,
+
+    userTimeZone,
+    usTimeZones,
+    use_en_time,
+
+    display_plus_sign_in_the_start
+  } from '../initials_inputs.js';
+\n`;
+
+  // Start the function definition string with the imports and exports
+  //let functionStr = importsAndExports;
+
+  let imports = new Set();
+
+  //let dynamicImports = importsAndExports;
+  let dynamicImports = 'import {\n';
+
+  // Check which parameters are used in the functionParams
+  Object.keys(functionParams).forEach(functionName => {
+    if (functionToProps[functionName]) { // Check if function is used
+      functionParams[functionName].match(/\b\w+\b/g).forEach(param => {
+        // Assuming all parameter names are valid JavaScript identifiers
+        imports.add(param);
+      });
+    }
+  });
+
+  imports.forEach(importName => {
+    // Add conditions to match importName with actual imports
+    dynamicImports += `  ${importName},\n`;
+  });
+
+  dynamicImports += '} from \'../initials_inputs.js\';\n \n \n';
+
+  // Import validation functions used
+  Object.keys(functionToProps).forEach(functionName => {
+    dynamicImports += `import { ${functionName} } from './Validators_renders_afterValidates/${functionName.replace('afterValidatefct_', 'valid_')}.js';\n`;
+  });
+
+  // Add other imports
+  dynamicImports += `import { addToast } from 'react-toast-notifications';\n`;
+  dynamicImports += `import { comments_messages } from '../Tools/comments_messages';\n\n`;
+
+  // Start building the function string
+  let functionStr = `${dynamicImports}export function afterValidatefct(isValid, oldvalue, row, prop, source, hot, userLocale, decimalSeparator, navigator_language, use_english_date_by_user_himeself_in_modal, commentsPlugin, isLoading, setNotification) {\n`;
+
+
+  // Add your existing logic to build the function body here
+
+  // End of function body
+  //functionStr += `}\n`; // Close the exported function
+
+
+
+  //let functionStr = `function afterValidatefct(isValid, oldvalue, row, prop, source, hot, userLocale, decimalSeparator, navigator_language, use_english_date_by_user_himeself_in_modal, commentsPlugin, isLoading, setNotification) {\n`;
   
   Object.entries(functionToProps).forEach(([functionName, propIndexes], index) => {
     // Join all prop indexes with logical OR for the condition
--- validationFunction_generated_automatically.js
+++ validationFunction_generated_automatically.js
@@ -1,8 +1,76 @@
-function afterValidatefct(isValid, oldvalue, row, prop, source, hot, userLocale, decimalSeparator, navigator_language, use_english_date_by_user_himeself_in_modal, commentsPlugin, isLoading, setNotification) {
-  if (prop == 1 || prop == 5) {
+import {
+  isValid,
+  oldvalue,
+  row,
+  prop,
+  source,
+  hot,
+  commentsPlugin,
+  decimalSeparator,
+  current,
+  userLocale,
+  navigator_language,
+  userTimeZone,
+  usTimeZones,
+  use_en_time,
+  use_english_date_by_user_himeself_in_modal,
+  setNotification,
+  isLoading,
+  emails_length_em,
+  onlynumbers_length_on,
+  phonenumbers_length_pn,
+  text_length_txt,
+  afterdigit_percentage_percperc,
+  smallafterdigit_percentage_percperc,
+  afterdigitsmallnb_percentage_percperc,
+  bignbpercent_percperc,
+  smallnbpercent_percperc,
+  decimalnumbers_toshow_withoutrenderer_inpercentage_percperc,
+  is_negativenb_accepted_percperc,
+  is_float_accepted_percperc,
+  display_plus_sign_in_the_start,
+  last_row_after_header,
+  currencyht_nbnb,
+  currencyht_toshow_nbnb,
+  afterdigit_nbnb,
+  smallafterdigit_nbnb,
+  afterdigitsmallnb_nbnb,
+  bignb_nbnb,
+  smallnb_nbnb,
+  decimalnumbers_toshow_withoutrenderer_innumbers_nbnb,
+  usegrouping_nbnb_if_true,
+  is_negativenb_accepted_nbnb,
+  currencyht_intint,
+  currencyht_toshow_intint,
+  afterdigit_intint,
+  smallafterdigit_intint,
+  afterdigitsmallnb_intint,
+  bignb_intint,
+  smallnb_intint,
+  decimalnumbers_toshow_withoutrenderer_innumbers_intint,
+  usegrouping_intint_if_true,
+  is_negativenb_accepted_intint,
+  is_float_accepted_intint,
+} from '../initials_inputs.js';
+ 
+ 
+import { afterValidatefct_onlynb } from './Validators_renders_afterValidates/valid_onlynb.js';
+import { afterValidatefct_email } from './Validators_renders_afterValidates/valid_email.js';
+import { afterValidatefct_dropdown } from './Validators_renders_afterValidates/valid_dropdown.js';
+import { afterValidatefct_phonenumber } from './Validators_renders_afterValidates/valid_phonenumber.js';
+import { afterValidatefct_date } from './Validators_renders_afterValidates/valid_date.js';
+import { afterValidatefct_percentage } from './Validators_renders_afterValidates/valid_percentage.js';
+import { afterValidatefct_amounts } from './Validators_renders_afterValidates/valid_amounts.js';
+import { afterValidatefct_text } from './Validators_renders_afterValidates/valid_text.js';
+import { afterValidatefct_integers } from './Validators_renders_afterValidates/valid_integers.js';
+import { addToast } from 'react-toast-notifications';
+import { comments_messages } from '../Tools/comments_messages';
+
+export function afterValidatefct(isValid, oldvalue, row, prop, source, hot, userLocale, decimalSeparator, navigator_language, use_english_date_by_user_himeself_in_modal, commentsPlugin, isLoading, setNotification) {
+  if (prop == 1) {
     afterValidatefct_onlynb(isValid, oldvalue, row, prop, source, hot, commentsPlugin, onlynumbers_length_on, setNotification);
   }
-  else if (prop == 2 || prop == 4) {
+  else if (prop == 2 || prop == 4 || prop == 5) {
     afterValidatefct_email(isValid, oldvalue, row, prop, source, hot, commentsPlugin, emails_length_em, setNotification);
   }
   else if (prop == 3) {
